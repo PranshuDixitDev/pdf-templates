f220b788cbe2bdaa71fd14d0427204bb1a1b06f3
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-01 17:32:48 +0530

    Added footer

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("pdf_topics.csv")
 
 for index, row in df.iterrows():
 	pdf.add_page()
 
-	pdf.set_font(family="Times", style="B", size=12)
+	# Set header
+
+	pdf.set_font(family="Times", style="B", size=24)
 	pdf.set_text_color(100, 100, 100)
 	pdf.cell(w=35, h=12, txt=row["Topic"], align="L", ln=1)
 	pdf.line(10, 25, 200, 25)
-# pdf.set_font(family="Times", size=14)
-# pdf.cell(w=30, h=12, txt="Write your thoughts!", align="L", ln=1)
+
+	# Set footer
+	pdf.ln(265)
+	pdf.set_font(family="Times", style="I", size=12)
+	pdf.set_text_color(180, 180, 180)
+	pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 	for i in range(row["Pages"] - 1):
 		pdf.add_page()
+
+		# Set footer
+		pdf.ln(277)
+		pdf.set_font(family="Times", style="I", size=12)
+		pdf.set_text_color(180, 180, 180)
+		pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
