fca9d7f328e90abf552d54ee2bd9f4a36bcbde10
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-01 17:41:33 +0530

    Add lines

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ for index, row in df.iterrows():
 	pdf.set_font(family="Times", style="B", size=24)
 	pdf.set_text_color(100, 100, 100)
 	pdf.cell(w=35, h=12, txt=row["Topic"], align="L", ln=1)
-	pdf.line(10, 25, 200, 25)
+
+	for y in range(20, 298, 10):
+		pdf.line(10, y, 200, y)
 
 	# Set footer
 	pdf.ln(265)
@@ -28,8 +30,11 @@ for index, row in df.iterrows():
 
 		# Set footer
 		pdf.ln(277)
-		pdf.set_font(family="Times", style="I", size=12)
+		pdf.set_font(family="Times", style="I", size=8)
 		pdf.set_text_color(180, 180, 180)
 		pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+		for y in range(20, 298, 10):
+			pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
