3dbb0c3c62c5097fa5648329c3777da9e5cf87aa
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-02-01 16:59:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF
+import pandas as pd
+
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("pdf_topics.csv")
+
+for index, row in df.iterrows():
+	pdf.add_page()
+
+	pdf.set_font(family="Times", style="B", size=12)
+	pdf.set_text_color(100, 100, 100)
+	pdf.cell(w=35, h=12, txt=row["Topic"], align="L", ln=1)
+	pdf.line(10, 25, 200, 25)
+# pdf.set_font(family="Times", size=14)
+# pdf.cell(w=30, h=12, txt="Write your thoughts!", align="L", ln=1)
+
+pdf.output("output.pdf")
